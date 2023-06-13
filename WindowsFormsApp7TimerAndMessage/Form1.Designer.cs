0832715ac35587942a407cb638b098a14cccf7a7
Emily <yi.hu17@education.nsw.gov.au>
2023-06-13 23:03:22 +1000

    Add project files.

---
+++ WindowsFormsApp7TimerAndMessage/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp7TimerAndMessage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAOne = new System.Windows.Forms.Button();
+            this.countdownTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblScreen = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnATwo = new System.Windows.Forms.Button();
+            this.btnAThree = new System.Windows.Forms.Button();
+            this.btnAFour = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAOne
+            // 
+            this.btnAOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAOne.Location = new System.Drawing.Point(489, 90);
+            this.btnAOne.Name = "btnAOne";
+            this.btnAOne.Size = new System.Drawing.Size(428, 82);
+            this.btnAOne.TabIndex = 0;
+            this.btnAOne.Text = "Answer 1";
+            this.btnAOne.UseVisualStyleBackColor = true;
+            this.btnAOne.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // countdownTimer
+            // 
+            this.countdownTimer.Interval = 1000;
+            this.countdownTimer.Tick += new System.EventHandler(this.countdownTimer_Tick);
+            // 
+            // lblScreen
+            // 
+            this.lblScreen.AutoSize = true;
+            this.lblScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScreen.Location = new System.Drawing.Point(1080, 94);
+            this.lblScreen.Name = "lblScreen";
+            this.lblScreen.Size = new System.Drawing.Size(106, 76);
+            this.lblScreen.TabIndex = 1;
+            this.lblScreen.Text = "00";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(344, 256);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 76);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnATwo
+            // 
+            this.btnATwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnATwo.Location = new System.Drawing.Point(489, 203);
+            this.btnATwo.Name = "btnATwo";
+            this.btnATwo.Size = new System.Drawing.Size(428, 78);
+            this.btnATwo.TabIndex = 3;
+            this.btnATwo.Text = "Answer 2";
+            this.btnATwo.UseVisualStyleBackColor = true;
+            this.btnATwo.Click += new System.EventHandler(this.btnATwo_Click);
+            // 
+            // btnAThree
+            // 
+            this.btnAThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnAThree.Location = new System.Drawing.Point(489, 319);
+            this.btnAThree.Name = "btnAThree";
+            this.btnAThree.Size = new System.Drawing.Size(428, 78);
+            this.btnAThree.TabIndex = 4;
+            this.btnAThree.Text = "Answer 3";
+            this.btnAThree.UseVisualStyleBackColor = true;
+            this.btnAThree.Click += new System.EventHandler(this.btnAThree_Click);
+            // 
+            // btnAFour
+            // 
+            this.btnAFour.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnAFour.Location = new System.Drawing.Point(489, 436);
+            this.btnAFour.Name = "btnAFour";
+            this.btnAFour.Size = new System.Drawing.Size(428, 78);
+            this.btnAFour.TabIndex = 5;
+            this.btnAFour.Text = "Answer 4";
+            this.btnAFour.UseVisualStyleBackColor = true;
+            this.btnAFour.Click += new System.EventHandler(this.btnAFour_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1428, 594);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnAFour);
+            this.Controls.Add(this.btnAThree);
+            this.Controls.Add(this.btnATwo);
+            this.Controls.Add(this.lblScreen);
+            this.Controls.Add(this.btnAOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAOne;
+        private System.Windows.Forms.Timer countdownTimer;
+        private System.Windows.Forms.Label lblScreen;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnATwo;
+        private System.Windows.Forms.Button btnAThree;
+        private System.Windows.Forms.Button btnAFour;
+    }
+}
+
---
+++ WindowsFormsApp7TimerAndMessage/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+
+namespace WindowsFormsApp7TimerAndMessage
+{
+    public partial class Form1 : Form
+    {
+        int seconds = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            seconds = 10;
+            countdownTimer.Start();
+        }
+
+        private void countdownTimer_Tick(object sender, EventArgs e)
+        {
+            lblScreen.Text = seconds--.ToString();
+            if (seconds < 0)
+            {
+                countdownTimer.Stop();
+                lblMessage.Text = "Sorry, you ran out of time";
+            }
+        }
+
+        private void btnAFour_Click(object sender, EventArgs e)
+        {
+            seconds = 10;
+            countdownTimer.Start();
+        }
+
+        private void btnATwo_Click(object sender, EventArgs e)
+        {
+            seconds = 10;
+            countdownTimer.Start();
+        }
+
+        private void btnAThree_Click(object sender, EventArgs e)
+        {
+            seconds = 10;
+            countdownTimer.Start();
+        }
+    }
+}
